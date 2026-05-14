4ec370bcb53a4400e1b92585e829daf2120b6b44
terefal <firstming23@gmail.com>
2026-05-14 21:50:25 +0800

    fix: 移除未使用的 GlideImagesPlugin import (不需要 Glide 依赖)

--- app/src/main/java/com/terefal/pdfaireader/chat/MarkdownRenderer.kt
+++ app/src/main/java/com/terefal/pdfaireader/chat/MarkdownRenderer.kt
@@ -8,7 +8,6 @@ import io.noties.markwon.ext.latex.JLatexMathPlugin
 import io.noties.markwon.ext.strikethrough.StrikethroughPlugin
 import io.noties.markwon.ext.tables.TablePlugin
 import io.noties.markwon.html.HtmlPlugin
-import io.noties.markwon.image.glide.GlideImagesPlugin
 
 object MarkdownRenderer {
 
